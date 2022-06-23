851eee7e9a5746aa06d7c3c6bb845a7bb61e170f
hemerson <hemerson.me@gmail.com>
2022-06-23 20:10:53 -0300

    Código limpo

--- 7_.py
+++ 7_.py
@@ -1,14 +1,3 @@
-# sistema de cadastro (para as roupas, etc)
-# salvar em arquivos texto
-
-# tipo de peça(M/F) > categorias (blusa, calça, saia) > nome da peça > tamanho > preço
-# infatil
-# Acessorios(Bone, chapeu, lenço, carteira, bolsas, cinto)
-
-
-
-# cada um vai ter o seu .txt
-
 import re
 import os
 from rich import print
@@ -20,6 +9,7 @@ class Roupa:
         self._categoria = categoria
         self._tamanho = tamanho
         self.verifica_tamanho_ou_categoria()
+
 
     @property
     def nome(self):
@@ -44,10 +34,11 @@ class Roupa_Masculina(Roupa):
     def __init__(self, nome, preco, categoria, tamanho):
         super().__init__(nome, preco, categoria, tamanho)
         self.manda_para_o_arquivo()
-    
+
+
     def manda_para_o_arquivo(self):
         lista_infor = [self.nome, self.preco, self.categoria, self.tamanho]
-        with open("roupa_masculina.txt", "a") as roup_masc:
+        with open("roupa_masculina.txt", "a", encoding="utf-8") as roup_masc:
             for infor in lista_infor:
                 roup_masc.write(str(infor) + ":")
             roup_masc.write("\n")
@@ -58,10 +49,11 @@ class Roupa_Feminina(Roupa):
     def __init__(self, nome, preco, categoria, tamanho):
         super().__init__(nome, preco, categoria, tamanho)
         self.manda_para_o_arquivo()
-    
+
+
     def manda_para_o_arquivo(self):
         lista_infor = [self.nome, self.preco, self.categoria, self.tamanho]
-        with open("roupa_feminina.txt", "a") as roup_femi:
+        with open("roupa_feminina.txt", "a", encoding="utf-8") as roup_femi:
             for infor in lista_infor:
                 roup_femi.write(str(infor) + ":")
             roup_femi.write("\n")
@@ -73,9 +65,10 @@ class Roupa_Infantil(Roupa):
         super().__init__(nome, preco, categoria, tamanho)
         self.manda_para_o_arquivo()
 
+
     def manda_para_o_arquivo(self):
         lista_infor = [self.nome, self.preco, self.categoria, self.tamanho]
-        with open("roupa_infantil.txt", "a") as roup_infan:
+        with open("roupa_infantil.txt", "a", encoding="utf-8") as roup_infan:
             for infor in lista_infor:
                 roup_infan.write(str(infor) + ":")
             roup_infan.write("\n")
@@ -88,9 +81,6 @@ class Acessorios(Roupa):
         self.verifica_tamanho_ou_categoria()
         self.manda_para_o_arquivo()
 
-    # @property
-    # def categoria(self):
-    #     return self._categoria.lower()
 
     def verifica_tamanho_ou_categoria(self):
         lista_categorias = ["bone", "chapeu", "lenço", "carteira", "bolsa", "cinto"]
@@ -99,7 +89,7 @@ class Acessorios(Roupa):
     
     def manda_para_o_arquivo(self):
         lista_infor = [self.nome, self.preco, self.categoria]
-        with open("acessorios.txt", "a") as acess:
+        with open("acessorios.txt", "a", encoding="utf-8") as acess:
             for infor in lista_infor:
                 acess.write(str(infor) + ":")
             acess.write("\n")
@@ -130,7 +120,6 @@ def registro_de_acessorios():
 
 while True:
     try:
-        # os.system("cls")
         escolha = int(input("[1] - Registrar\n[2] - Excluir registro\n"))
     except:
         os.system("cls")
@@ -140,7 +129,6 @@ while True:
         while True:
 
             try:
-                # os.system("cls")
                 print("Escolha a categoria")
                 registrar = int(input("[1] - Roupa Masculina\n[2] - Roupa Feminina\n[3] - Roupa Infantil\n[4] - Acessorios\n[5] - Voltar\n"))
             except:
@@ -150,27 +138,18 @@ while True:
             if registrar == 1:
                 registro_de_roupas()
                 nova_roup_masc = Roupa_Masculina(nome, preco, categoria, tamanho)
-                # lista_infor = [nova_roup_masc.nome, nova_roup_masc.preco, nova_roup_masc.categoria, nova_roup_masc.tamanho]
-                # print(lista_infor)
-                # with open("roupa_masculina.txt", "a") as roup_masc:
-                #     for infor in lista_infor:
-                #         roup_masc.write(str(infor) + ":")
-                #     roup_masc.write("\n")
             
             elif registrar == 2:
                 registro_de_roupas()
                 nova_roup_femin = Roupa_Feminina(nome, preco, categoria, tamanho)
-                # print(f"{nova_roup_femin.nome}\n{nova_roup_femin.preco}\n{nova_roup_femin.categoria}\n{nova_roup_femin.tamanho}")
             
             elif registrar == 3:
                 registro_de_roupas()
                 nova_roup_infa = Roupa_Infantil(nome, preco, categoria, tamanho)
-                # print(f"{nova_roup_infa.nome}\n{nova_roup_infa.preco}\n{nova_roup_infa.categoria}\n{nova_roup_infa.tamanho}")
 
             elif registrar == 4:
                 registro_de_acessorios()
                 novo_acess = Acessorios(nome, preco, categoria)
-                # print(f"{novo_acess.nome}\n{novo_acess.preco}\n{novo_acess.categoria}")
 
             elif registrar == 5:
                 break
@@ -185,15 +164,3 @@ while True:
         os.system("cls")
         print("[on red]Digite uma opção válida[/]")
         continue
-
-roup_mac = Roupa_Masculina("blusa Azul", 22.49, "BLuSa", "m")
-print(f"{roup_mac.nome}\n{roup_mac.preco}\n{roup_mac.categoria}\n{roup_mac.tamanho}")
-
-roup_femi = Roupa_Feminina("vestido rosa", 72.49, "VESTIDO", "m")
-print(f"{roup_femi.nome}\n{roup_femi.preco}\n{roup_femi.categoria}\n{roup_femi.tamanho}")
-
-roup_infa = Roupa_Infantil("Macacao roxo", 422.49, "macacão", "pp")
-print(f"{roup_infa.nome}\n{roup_infa.preco}\n{roup_infa.categoria}\n{roup_infa.tamanho}")
-
-ace = Acessorios("pulseira", 12.09, "pulseira")
-print(f"{ace.nome}\n{ace.preco}\n{ace.categoria}")
